e6d873df49c9af88578b8744a8837ce7e1b3799a
Flipsid3 team <eugene.korotkov@gmail.com>
2015-06-03 11:43:02 +0300

    Update

---
+++ 0-helloex/Hello.java
@@ -0,0 +1,8 @@
+package com.flipsid3.printapp;
+import Printer.java;
+
+class Hello {
+    public static void main(String[] args) {
+       Printer.print();
+    }
+}
---
+++ 0-helloex/Printer.java
@@ -0,0 +1,8 @@
+package com.flipsid3.printapp;
+
+
+class Printer {
+    public static void print() {
+        System.out.println("Hello World!");
+    }
+}
